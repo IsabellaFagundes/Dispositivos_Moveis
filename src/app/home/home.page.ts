8633841e4c27d0d46ee62745b6bb8cb78a347605
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-04-28 14:18:53 -0300

    criando alerta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 interface Transaction {
   date: Date;
@@ -14,10 +15,14 @@ export class HomePage {
 
   public counter = 0;
   public selectedValue = 0;
-  public transactions: Transaction [] = [];
+  public transactions: Transaction[] = [];
 
-  public deposit(){
-    this.counter+= this.selectedValue;
+  constructor(private alertController: AlertController) {
+
+  }
+
+  public deposit() {
+    this.counter += this.selectedValue;
     this.transactions.unshift({ //insere um novo elemento no começo da lista
       amount: this.selectedValue,
       date: new Date()
@@ -25,5 +30,53 @@ export class HomePage {
     this.selectedValue = 0;
   }
 
+  /*
+  public async confirmReset(){
+    const alert = await this.alertController.create({
+      header:'Tem certeza?',
+      message:'Essa operação não poderá ser desfeita!',
+      buttons:[
+        {
+          text: 'Sim',
+          handler: () => {
+            this.transactions.unshift ({
+              amount: -this.counter,
+              date: new Date()
+            });
+            this.counter = 0;
+            this.selectedValue =0;
+          }
+        }, 
+        'Me enganei'
+      ]
+    });
+    alert.present();
+  } */
+
+  public async confirmWipe() {
+    const alert = await this.alertController.create({
+      header: 'Tem certeza?',
+      message: 'Essa operação não poderá ser desfeita!',
+      buttons: [
+        {
+          text: 'Sim',
+          handler: () => {
+            this.wipeAccount();
+          }
+        },
+        'Me enganei'
+      ]
+    });
+    alert.present();
+  }
+
+  private wipeAccount(){ //limpar a conta
+    this.transactions.unshift({ //faz a operação de sacar tudo aparecer no "extrato"
+      amount: -this.counter,
+      date: new Date()
+    });
+    this.counter = 0; //"limpa" a conta
+    this.selectedValue = 0;
+  }
 
 }
