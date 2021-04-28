47de6c5def65794841dbc5295cd3b7647f43cbe1
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-04-28 00:50:39 -0300

    Contador_Conta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public counter = 0;
+  public maxCounter = 0;
+  public selectedValue = 0;
+
+  public increment(){
+    this.counter++;
+    if (this.counter > this.maxCounter){ 
+      this.maxCounter = this.counter;  //se o contador foi maior que o contador máximmo, contador máximo passa a ser igual o contador
+    } // ou this.maxCounter = Math.max(this.counter, this.maxCounter); -> o maxCounter vai ser o maior numero entre maxCounter e Counter
+  }
+
+  public decrement(){
+    this.counter --;
+    if(this.counter <= 0){
+      this.counter = 0; //se o contador for menor que 0, ele fica igual a 0
+    }// ou this.counter = Math.max(this.counter -1, 0); -> o maxCounter vai ser o maior numero entre e o valor decrementado e 0
+    
+  }
+
+  public reset(){
+    this.counter = 0;
+  }
+
+  public incrementConta(){
+    this.counter+= this.selectedValue;
+    this.selectedValue = 0;
+    if (this.counter > this.maxCounter){ 
+      this.maxCounter = this.counter;  //se o contador foi maior que o contador máximmo, contador máximo passa a ser igual o contador
+    } // ou this.maxCounter = Math.max(this.counter, this.maxCounter); -> o maxCounter vai ser o maior numero entre maxCounter e Counter
+  }
+
+  public decrementConta(){
+    this.counter = Math.max(this.counter-this.selectedValue, 0);
+    this.selectedValue = 0;
+  }
+
+  public resetConta(){
+    this.counter = 0;
+  }
 
 }
